da79933e88e87aba9d302697a556136be93b100c
Alejandro <aleludenat19@gmail.com>
2025-05-26 23:23:43 -0500

    Delete headers

--- api/ecommerce-api.js
+++ api/ecommerce-api.js
@@ -3,11 +3,7 @@ const URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getProducts() {
    try {
-      const response = await axios.get(`${URL}/product/all`, {
-         headers: {
-            'ngrok-skip-browser-warning': 'true'
-         }
-      });
+      const response = await axios.get(`${URL}/product/all`);
       if (!response) throw new Error("Not find any products");
       return response.data;
    } catch (error) {
